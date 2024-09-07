8f39629eb800ee11b98e5bb5fe402c66459805c7
Francisco Santos <franciscosantos@moobitech.com.br>
2024-09-07 14:21:58 -0300

    Feature: Reformulando o footer.

--- web/src/app/components/footer/footer.component.ts
+++ web/src/app/components/footer/footer.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import {
+  faBook,
+  faBookOpen,
+  faCartShopping, faEnvelope, faHome, faList,
+  faListCheck, faPhone, faSquarePhone,
+  faUser,
+  faUserGraduate, faUsers,
+} from "@fortawesome/free-solid-svg-icons";
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {RouterLink} from "@angular/router";
+import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [
+    FaIconComponent,
+    RouterLink,
+    NgOptimizedImage
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
   ano = new Date().getFullYear();
+  email = "papelariaevariedadesideal@hotmail.com"
+
+  protected readonly faUser = faUser;
+  protected readonly faUserGraduate = faUserGraduate;
+  protected readonly faCartShopping = faCartShopping;
+  protected readonly faListCheck = faListCheck;
+  protected readonly faBook = faBook;
+  protected readonly faBookOpen = faBookOpen;
+  protected readonly faList = faList;
+  protected readonly faUsers = faUsers;
+  protected readonly faHome = faHome;
+  protected readonly faEnvelope = faEnvelope;
+  protected readonly faSquarePhone = faSquarePhone;
+  protected readonly faPhone = faPhone;
 }
